47a4265d446de2e6e83af2416cbcd73ee0957da5
zhangchengxi <orezsilence@163.com>
2017-06-26 09:41:50 +0800

    add

--- src/test/java/com/daniel/onepage/repository/PageRepositoryTest.java
+++ src/test/java/com/daniel/onepage/repository/PageRepositoryTest.java
@@ -13,7 +13,6 @@ public class PageRepositoryTest extends OnePageApplicationTests {
     @Autowired
     private PageRepository pageRepository;
 
-
     @Test
     public void testFindAll() {
         Iterable<Page> all = pageRepository.findAll();
